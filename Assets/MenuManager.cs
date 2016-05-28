1bec4bfcf45dbe892af8a66ea3a779cf050ae97d
JoseBaltazar <joseba_97@hotmail.com>
2016-05-28 01:43:24 -0500

    Adicion de proyecto inicial

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+    public GameObject loadImage;
+
+    public void NewGame (string sceneName)
+    {
+        //GameObject.Find("Dif").SetActive(true);
+        loadImage.SetActive(true);
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Options(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    IEnumerator LoadScene()
+    {
+        yield return new WaitForSeconds(5);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+
+        PlayerMove();
+	
+	}
+
+    public void PlayerMove()
+    {
+        if (Input.GetKeyDown(KeyCode.A)&&transform.position.x>0)
+        {
+            gameObject.transform.Translate(-1, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            gameObject.transform.Translate(1, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            gameObject.transform.Translate(0, 0, 1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S)&& transform.position.z>0)
+        {
+            gameObject.transform.Translate(0, 0, -1);
+        }
+
+    }
+}
