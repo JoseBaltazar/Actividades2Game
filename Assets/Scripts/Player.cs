3ae90b1cec811ebce1f274498f932d532ff092b3
YoshiLui <yoshi.lui@gmail.com>
2016-05-28 03:01:27 -0500

    Modifique el script Player agregandole los efectos de sonido y agregue la carpeta AudioClips

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,39 +3,85 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
+    public int vol = 10;
+    public AudioClip playerStep;
+    public AudioClip lifeUp;
+    public AudioClip lifeDown;
+    public AudioClip die;
+    private int lifes;
+    private AudioSource source;
+    
 
 
-	void Start () {
-	
-	}
-	
-	void Update () {
 
-        PlayerMove();
-	
-	}
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+
+        lifes = 3;
+
+
+
+    }
+
+    void Update()
+    {
+
+
+
+        Debug.Log("Vidas: " + lifes);
+
+        if (lifes <= 0) { 
+        Debug.Log("GG");
+        source.PlayOneShot(die, vol);
+      }
+        else
+        {
+            PlayerMove();
+        }
+    }
 
     public void PlayerMove()
     {
-        if (Input.GetKeyDown(KeyCode.A)&&transform.position.x>0)
+        if (Input.GetKeyDown(KeyCode.A) && transform.position.x > 0)
         {
             gameObject.transform.Translate(-1, 0, 0);
+            source.PlayOneShot(playerStep, vol);
         }
 
         if (Input.GetKeyDown(KeyCode.D))
         {
             gameObject.transform.Translate(1, 0, 0);
+            source.PlayOneShot(playerStep, vol);
         }
 
         if (Input.GetKeyDown(KeyCode.W))
         {
+           
             gameObject.transform.Translate(0, 0, 1);
+            source.PlayOneShot(playerStep, vol);
         }
 
-        if (Input.GetKeyDown(KeyCode.S)&& transform.position.z>0)
+        if (Input.GetKeyDown(KeyCode.S) && transform.position.z > 0)
         {
+
             gameObject.transform.Translate(0, 0, -1);
+            source.PlayOneShot(playerStep, vol);
         }
 
+    }
+
+    void OntriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            source.PlayOneShot(lifeDown, vol);
+            lifes--;
+        }
+        if (other.gameObject.tag == "MasVida")
+        {
+            source.PlayOneShot(lifeUp, vol);
+            lifes++;
+        }
     }
 }
